5a4977ca4440968ff3538772e76a64b686f4bed6
BenediktWoermann <benedikt1.woermann@gmail.com>
2022-06-08 12:06:50 +0200

    added cloud animation function

--- src/background.cpp
+++ src/background.cpp
@@ -2,8 +2,25 @@
 
 #include "display.h"
 #include <FastLED.h>
+#include "OTA.h"
 
 #define DEBUGGING 1
+
+void cloudbg(float speed, int brightness, int scale){
+    unsigned int offset = int(millis()*speed);
+
+    for(int col = 0; col<ledColumns; col++){
+        for(int row = 0; row<ledRows; row++){
+            if(display[row][col] != NULL){
+                uint8_t noise = inoise8(scale * row, scale * col + offset);
+                noise = qsub8(noise, 10);
+                noise = qadd8(noise,scale8(noise,100));
+
+                *display[row][col] = CHSV(160, noise, brightness);
+            }
+        }
+    }
+}
 
 void rainbowbg(float speed, int brightness){
     unsigned int baseHue = int(millis()*speed)%256;
--- src/background.h
+++ src/background.h
@@ -4,5 +4,6 @@
 extern CRGB leds[ledQuantity];
 extern CRGB* display[ledRows][ledColumns];
 
+void cloudbg(float speed, int brightness, int scale);
 void rainbowbg(float speed, int brightness);
 void staticbg(long color);
